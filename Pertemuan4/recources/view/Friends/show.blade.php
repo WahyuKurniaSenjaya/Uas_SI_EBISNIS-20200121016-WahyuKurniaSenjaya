20b916c1a90fe10e94972d9aa07c06fe3e5aec1c
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-04-24 14:38:35 +0700

    commit 5

---
+++ Pertemuan4/recources/view/Friends/show.blade.php
@@ -0,0 +1,12 @@
+@extends ('layout.app')
+
+@section('tittle', 'Cobaaaa')
+
+@section('content')
+<div class="card">
+    <h3?>Nama Teman : {{ $friend['nama'] }}</h3>
+    <h3?>No Tlp Teman : {{ $friend['no_tlp'] }}</h3>
+    <h3?>Alamat Teman : {{ $friend['alamat'] }}</h3>
+</div>
+</div>
+@endsection

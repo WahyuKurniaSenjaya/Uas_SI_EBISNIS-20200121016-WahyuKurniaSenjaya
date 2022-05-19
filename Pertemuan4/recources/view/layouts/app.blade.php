519ab582e59be2802bd14d5c9a748593d324a329
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-05-19 07:44:55 +0700

    commit 7

--- Pertemuan4/recources/view/layouts/app.blade.php
+++ Pertemuan4/recources/view/layouts/app.blade.php
@@ -8,11 +8,13 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>@yield('title')</title>
+    <title>>@yield('title')</title>
   </head>
   <body>
-      <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+
+  <div class="container">
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Laravel Pertama</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,14 +24,14 @@
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="/">Home</a>
         <a class="nav-link" href="/friends">Friends</a>
-       
+        <a class="nav-link" href="/groups">Groups</a>
+      </div>
     </div>
   </div>
 </nav>
-<a href="/friends/create" class="card-link btn-primary">Tambah Teman</a>
 
-     @yield('content')
 
+  @yield('content')
 
 </div>
 

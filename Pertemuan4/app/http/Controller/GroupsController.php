9a5ea8b9e31365c8b7d85088352b4ca36a863eec
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-05-19 07:32:23 +0700

    commit 7

---
+++ Pertemuan4/app/http/Controller/GroupsController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Groups;
+
+class GroupsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $groups = Groups::orderBy('id','desc')->paginate(3);
+        return view('groups.index', compact('groups'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('groups.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      // Validate the request...
+      $request->validate([
+        'name' => 'required|unique:groups|max:255',
+        'description' => 'required',
+    ]);
+
+    $groups = new groups;
+
+    $groups->name = $request->name;
+    $groups->description = $request->description;
+
+    $groups->save();
+
+    return redirect('/groups');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $groups = groups::where('id', $id)->first();
+        return view('groups.show',['groups' => $groups]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $groups = groups::where('id', $id)->first();
+        return view('groups.edit',['groups' => $groups]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         // Validate the request...
+         $request->validate([
+            'name' => 'required|unique:groups|max:255',
+            'description' => 'required',
+        ]);
+        
+        groups::find($id)->update([
+            'name' => $request->name,
+            'description' => $request->description
+        ]);
+        return redirect('/groups');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        groups::find($id)->delete();
+        return redirect('/groups');
+    }
+}

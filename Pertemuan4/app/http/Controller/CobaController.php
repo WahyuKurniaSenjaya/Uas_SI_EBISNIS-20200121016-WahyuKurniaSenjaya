cd168cb9871b1ab31ccd370d255e9fcb3e27642f
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-05-19 07:29:38 +0700

    commit 7

--- Pertemuan4/app/http/Controller/CobaController.php
+++ Pertemuan4/app/http/Controller/CobaController.php
@@ -3,90 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Friends;
+use App\Models\friends;
 
 class CobaController extends Controller
 {
-    /*
     public function index()
     {
-            return 'test berhasil';
+        $friends = friends::orderBy('id','desc')->paginate(3);
+        return view('friends.index', compact('friends'));
     }
-    public function urutan($ke)
+    public function create()
     {
-        $friends = Friends::paginate(3);
-            return view ('friend', compact('friends'));
-    }
-    public function coba($ke)
-    {
-            return view ('coba', ['ke' => $ke]);
-    }
-*/
-
-
-    public function index ()
-    {
-        $friends = Friends::orderBy('id', 'desc')->paginate(3);
-        return view ('friends.index', compact('friends'));
-    }
-
-    public function create ()
-    {
-        return view ('friends.create');
+        return view('friends.create');
+        
     }
     public function store(Request $request)
     {
         // Validate the request...
-
         $request->validate([
             'nama' => 'required|unique:friends|max:255',
-            'no_telp' => 'required|numeric',
+            'no_tlp' => 'required | numeric',
             'alamat' => 'required',
         ]);
  
         $friends = new Friends;
  
         $friends->nama = $request->nama;
-        $friends->no_telp = $request->no_telp;
+        $friends->no_tlp = $request->no_tlp;
         $friends->alamat = $request->alamat;
  
         $friends->save();
 
         return redirect('/');
     }
-
     public function show($id)
     {
         $friends = friends::where('id', $id)->first();
-        return view('friends.show', ['friend'=> $friends]);
+        return view('friends.show',['friends' => $friends]);
     }
     public function edit($id)
     {
         $friends = friends::where('id', $id)->first();
-        return view('friends.edit', ['friend'=> $friends]);
+        return view('friends.edit',['friends' => $friends]);
     }
-
     public function update(Request $request, $id)
     {
-
+        // Validate the request...
         $request->validate([
             'nama' => 'required|unique:friends|max:255',
-            'no_telp' => 'required|numeric',
+            'no_tlp' => 'required | numeric',
             'alamat' => 'required',
         ]);
- 
+        
         friends::find($id)->update([
             'nama' => $request->nama,
-            'no_telp' => $request->no_telp,
+            'no_tlp' => $request->no_tlp,
             'alamat' => $request->alamat
         ]);
-
         return redirect('/');
-
     }
 
-    public function destroy($id)
-    {
+    public function destroy(Request $request, $id){
         friends::find($id)->delete();
         return redirect('/');
     }

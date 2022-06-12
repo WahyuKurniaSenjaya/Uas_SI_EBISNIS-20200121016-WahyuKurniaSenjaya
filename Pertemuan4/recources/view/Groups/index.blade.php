6b4661ac9b9ffc1daecb4791745917642820ed07
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-06-12 12:34:22 +0700

    commit 8

--- Pertemuan4/recources/view/Groups/index.blade.php
+++ Pertemuan4/recources/view/Groups/index.blade.php
@@ -11,17 +11,29 @@
     <a href="/groups/{{$group['id']}}" class="card-title">{{$group['name']}}</a>
     <p class="card-text">{{$group['description']}}</p>
       <hr>
-        <a href="" class="card-link btn-primary">Tambah Anggota Teman</a>
-          @foreach ($group->friends as $friend)
-          <li?> {{$friend->nama}} </li>
-          @endforeach
+        <a href="/groups/addmember/{{$group['id']}}" class="card-link btn-primary">Tambah Anggota Teman</a>
+        <ul class="list-group">  
+        @foreach ($group->friends as $friend)
+        
 
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              {{$friend->nama}}}
+              <form action="/groups/delteaddmember/{{ $group->'id'}}" method="POST">
+                @csrf
+                @method('PUT')
+              <button type="submit" class="bedge card-link btn-danger">x</button>
+          </form>
+          </li>
+
+          @endforeach
+          </ul>
       <hr>
     <a href="/groups/{{$group['id']}}/edit" class="card-link btn-warning">Edit Group</a>
     <form action="/groups{{ $group['id'] }}" method="POST">
       @csrf
       @method('DELETE')
     <button class="card-link btn-danger">Delete Group</button>
+</form>
   </div>
 </div>
 
